c5c7142ec31321cd3a0f463a9ec61bf4b4e8a693
Jasen <jasen.miyamoto@gmail.com>
2024-04-02 15:34:58 -0400

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="", page_count=None):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._title
+    
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if (not type(new_page_count) is int):
+            print("page_count must be an integer")
+        else:
+            self._title = new_page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=None, condition=""):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, new_size):
+        if (not type(new_size) is int):
+            print("size must be an integer")
+        else:
+            self._size = new_size
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
+    
